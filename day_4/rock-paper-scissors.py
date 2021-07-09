0c093516664da1497c8aef0d4a727653d346c7ab
Brent <mina512@live.com>
2021-07-08 23:05:09 -0500

    remove redundant string

--- day_4/rock-paper-scissors.py
+++ day_4/rock-paper-scissors.py
@@ -29,8 +29,6 @@ scissors = """
 game_images = [rock, paper, scissors]
 comp_choice = randint(0, 2)
 
-"1 < 0, 2 < 1, 0 < 2"
-
 player_choice = int(
     input("What do you choose? Type 0 for Rock, 1 for Paper, or 2 for Scissors.\n")
 )
