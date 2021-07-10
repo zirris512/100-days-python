efe54429fb3f94cc937cf6aacbf31123d0cdea7e
Brent <mina512@live.com>
2021-07-09 22:45:23 -0500

    add day 5 mini project

---
+++ day_5/password-generator.py
@@ -0,0 +1,80 @@
+# Random Password Generator
+from random import shuffle, choice
+
+letters = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+]
+numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+symbols = ["!", "#", "$", "%", "&", "(", ")", "*", "+"]
+
+print("Welcome to the PyPassword Generator!\n")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+password_list = []
+total_length = nr_letters + nr_symbols + nr_numbers
+
+for n in range(1, total_length + 1):
+    if n <= nr_letters:
+        password_list.append(choice(letters))
+    elif n <= nr_letters + nr_symbols:
+        password_list.append(choice(symbols))
+    else:
+        password_list.append(choice(numbers))
+
+shuffle(password_list)
+
+new_password = "".join(password_list)
+print(f"\nYour new password is: {new_password}")
