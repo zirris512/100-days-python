08997bec41a88d2e63a1bb0ebb7f1faa4a95ef08
Brent <mina512@live.com>
2021-07-20 22:02:41 -0500

    add day 15 coffee maker project using oop

---
+++ day_15/main.py
@@ -0,0 +1,33 @@
+from os import system, name
+
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+
+def clear():
+    if name == "nt":
+        system("cls")
+    else:
+        system("clear")
+
+
+coffee_items = Menu()
+maker = CoffeeMaker()
+cash_machine = MoneyMachine()
+is_on = True
+
+while is_on:
+    user_choice = input(f"What would you like? ({coffee_items.get_items()}) ").lower()
+
+    if user_choice == "off":
+        is_on = False
+    elif user_choice == "report":
+        maker.report()
+        cash_machine.report()
+    else:
+        menu_item = coffee_items.find_drink(user_choice)
+        if menu_item:
+            if maker.is_resource_sufficient(menu_item):
+                if cash_machine.make_payment(menu_item.cost):
+                    maker.make_coffee(menu_item)
