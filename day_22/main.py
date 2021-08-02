842ecffc3f6de632aaa2a4378f5cdacf4fcec38e
Brent <mina512@live.com>
2021-08-02 12:27:47 -0500

    add day 22 pong game

---
+++ day_22/main.py
@@ -0,0 +1,56 @@
+from turtle import Screen
+import time
+
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Score
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("Pong")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+score = Score()
+
+screen.onkeypress(r_paddle.move_up, "Up")
+screen.onkeypress(r_paddle.move_down, "Down")
+screen.onkeypress(l_paddle.move_up, "w")
+screen.onkeypress(l_paddle.move_down, "s")
+screen.listen()
+
+game_is_on = True
+
+while game_is_on:
+    time.sleep(ball.ball_speed)
+    screen.update()
+    ball.move()
+
+    # Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Detect collision with paddles
+    if (
+        ball.distance(r_paddle) < 50
+        and ball.xcor() > 320
+        or ball.distance(l_paddle) < 50
+        and ball.xcor() < -320
+    ):
+        ball.bounce_x()
+
+    # Detect r_paddle miss
+    if ball.xcor() > 380:
+        ball.reset_position()
+        score.l_point()
+
+    # Detect l_paddle miss
+    if ball.xcor() < -380:
+        ball.reset_position()
+        score.r_point()
+
+
+screen.exitonclick()
