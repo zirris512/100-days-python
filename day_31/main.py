85fb95dc30186eddc0d6e0ab4f0a1552ad115e7e
Brent <mina512@live.com>
2021-09-21 16:08:20 -0500

    add day 31 flash card app

---
+++ day_31/main.py
@@ -0,0 +1,66 @@
+from tkinter import *
+import pandas
+from random import choice
+
+BACKGROUND_COLOR = "#B1DDC6"
+current_card = {}
+
+try:
+    word_records = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    word_records = pandas.read_csv("data/french_words.csv")
+
+words_dict = word_records.to_dict(orient="records")
+
+
+def next_card():
+    global current_card, flip_timer
+
+    window.after_cancel(flip_timer)
+    current_card = choice(words_dict)
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=card_front_img)
+    flip_timer = window.after(3000, func=flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_img)
+
+
+def is_known():
+    words_dict.remove(current_card)
+    data = pandas.DataFrame(words_dict)
+    data.to_csv("data/words_to_learn.csv", index=False)
+
+    next_card()
+
+
+window = Tk()
+window.title("Flashy")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+flip_timer = window.after(3000, func=flip_card)
+
+canvas = Canvas(width=800, height=526)
+card_front_img = PhotoImage(file="images/card_front.png")
+card_back_img = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front_img)
+card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(row=0, column=0, columnspan=2)
+
+cross_image = PhotoImage(file="images/wrong.png")
+unknown_button = Button(image=cross_image, highlightthickness=0, command=next_card)
+unknown_button.grid(row=1, column=0)
+
+check_image = PhotoImage(file="images/right.png")
+known_button = Button(image=check_image, highlightthickness=0, command=is_known)
+known_button.grid(row=1, column=1)
+
+next_card()
+
+window.mainloop()
